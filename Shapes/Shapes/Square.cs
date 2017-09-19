ec17ca7c5e62ac7e8160c823b811ab7e4df843f4
noiseless-fan <kozipopik@gmail.com>
2017-09-19 23:24:47 +0700

    IShape + Square

---
+++ Shapes/Shapes/Square.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork_Lyulyaev
+{
+	class Square: IShape
+	{
+		private const int Sides = 4;
+		private int Width;
+		private int Height;
+
+		public double GetWidth()
+		{
+			return Width;
+		}
+
+		public double GetHeight()
+		{
+			return Height;
+		}
+
+		public Square(int sideSize)
+		{
+			Width = Height = sideSize;
+		}
+
+		public double GetPerimeter()
+		{
+			return Height * Sides;
+		}
+
+		public double GetArea()
+		{
+			return Math.Pow(Width, 2);
+		}
+	}
+}
