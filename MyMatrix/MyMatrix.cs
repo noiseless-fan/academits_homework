a72e800c7a51e90340b3d19df275cf4efd3984b4
noiseless-fan <kozipopik@gmail.com>
2017-10-18 22:41:51 +0700

    ------------

--- MyMatrix/MyMatrix.cs
+++ MyMatrix/MyMatrix.cs
@@ -3,11 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using using HomeWork_Lyulyaev;
+
+class HomeWork_Lyulyaev.Vector;
 
 namespace HomeWork_Lyulyaev
 {
 	public class MyMatrix
 	{
-		
+		public static void Main()
+		{
+		}
 	}
 }
